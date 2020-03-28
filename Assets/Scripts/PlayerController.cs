5c4d70b7a5d923e6fa7b2e1c2d50fc637905da00
Joseph Adrian <joseph.danger.adrian@gmail.com>
2020-03-27 20:37:46 -0400

    More work on player controller

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,12 +5,15 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     Rigidbody body;
-    public float testStrength;
+    public float moveSpeed, jumpForce;
+    public float jumpCD;
+    float jumpTimer;
 
     // Start is called before the first frame update
     void Start()
     {
         body = GetComponent<Rigidbody>();
+        jumpTimer = 0;
     }
 
     // Update is called once per frame
@@ -19,25 +22,40 @@ public class PlayerController : MonoBehaviour
 
         Vector3 moveDirection = new Vector3();
 
-        if(Input.GetKey(KeyCode.W))
+        gameObject.transform.Rotate(0, Input.GetAxis("Horizontal"), 0);
+
+        if (Input.GetKey(KeyCode.W))
+            moveDirection += gameObject.transform.TransformDirection(new Vector3(0, 0, 1));
+        
+        if(Input.GetKey(KeyCode.S))
+            moveDirection += gameObject.transform.TransformDirection(new Vector3(0, 0, -1 ));
+        
+        if(Input.GetKey(KeyCode.A))
+            moveDirection += gameObject.transform.TransformDirection(new Vector3(-1, 0, 0));
+        
+        if(Input.GetKey(KeyCode.D))
+            moveDirection += gameObject.transform.TransformDirection(new Vector3(1, 0, 0));
+        
+
+        if(moveDirection.x != 0 && moveDirection.y != 0)
+            moveDirection /= Mathf.Sqrt(2);
+
+        moveDirection *= moveSpeed;
+        gameObject.transform.position += moveDirection;
+        if (jumpTimer <= 0)
         {
-            moveDirection += gameObject.transform.TransformDirection(new Vector3(0, 0, 1 * testStrength));
+            if (Input.GetKey(KeyCode.Space))
+            {
+                jumpTimer = jumpCD;
+                body.AddForce(new Vector3(0, jumpForce, 0));
+            }
+        }
+        else if(jumpTimer > 0)
+        {
+            jumpTimer -= Time.deltaTime;
         }
 
-        if(Input.GetKey(KeyCode.S))
-        {
-            moveDirection += gameObject.transform.TransformDirection(new Vector3(0, 0, -1 * testStrength));
         }
-        if(Input.GetKey(KeyCode.A))
-        {
-            moveDirection += gameObject.transform.TransformDirection(new Vector3(-1 * testStrength, 0, 0));
-        }
-        if(Input.GetKey(KeyCode.D))
-        {
-            moveDirection += gameObject.transform.TransformDirection(new Vector3(1 * testStrength, 0, 0));
-        }
-            body.AddForce(moveDirection);
-    }
 
 
 }
